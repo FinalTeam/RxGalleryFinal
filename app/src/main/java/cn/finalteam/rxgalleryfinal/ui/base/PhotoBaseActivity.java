356a55b4e0c613c6000f562a09948239159ca977
卢成龙 <1053998178@qq.com>
2016-05-03 15:54:48 +0800

    1

--- app/src/main/java/cn/finalteam/rxgalleryfinal/ui/base/PhotoBaseActivity.java
+++ app/src/main/java/cn/finalteam/rxgalleryfinal/ui/base/PhotoBaseActivity.java
@@ -13,6 +13,5 @@ public abstract class PhotoBaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 }
