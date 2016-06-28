1cfe8642226241e0c19ae1a3f9bceb23d79927bc
彭建波 <pengjianbo@finalteam.cn>
2016-06-28 11:32:14 +0800

    Add bucket bean.

---
+++ library/src/main/java/cn/finalteam/rxgalleryfinal/bean/BucketBean.java
@@ -0,0 +1,27 @@
+package cn.finalteam.rxgalleryfinal.bean;
+
+/**
+ * Desction:文件夹信息
+ * Author:pengjianbo
+ * Date:16/6/9 下午2:47
+ */
+public class BucketBean {
+    private String bucketId;
+    private String bucketName;
+
+    public String getBucketId() {
+        return bucketId;
+    }
+
+    public void setBucketId(String bucketId) {
+        this.bucketId = bucketId;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+}
