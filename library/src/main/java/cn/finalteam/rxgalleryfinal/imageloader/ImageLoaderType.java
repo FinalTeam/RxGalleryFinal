7ece94582b7aac16773da4ee84b8700f1bd8f504
彭建波 <pengjianbo@finalteam.cn>
2016-07-25 23:42:12 +0800

    添加picasso图片加载器

---
+++ library/src/main/java/cn/finalteam/rxgalleryfinal/imageloader/ImageLoaderType.java
@@ -0,0 +1,10 @@
+package cn.finalteam.rxgalleryfinal.imageloader;
+
+/**
+ * Desction:
+ * Author:pengjianbo
+ * Date:16/7/25 下午3:36
+ */
+public enum ImageLoaderType {
+    PICASSO
+}
---
+++ library/src/main/java/cn/finalteam/rxgalleryfinal/imageloader/PicassoImageLoader.java
@@ -0,0 +1,30 @@
+package cn.finalteam.rxgalleryfinal.imageloader;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+import cn.finalteam.rxgalleryfinal.ui.widget.FixImageView;
+
+/**
+ * Desction:
+ * Author:pengjianbo
+ * Date:16/6/17 下午1:23
+ */
+public class PicassoImageLoader implements AbsImageLoader {
+
+    @Override
+    public void displayImage(Object context, String path, FixImageView imageView, Drawable defaultDrawable, int width, int height) {
+        Context ctx = (Context) context;
+        Picasso.with(ctx)
+                .load(new File(path))
+                .placeholder(defaultDrawable)
+                .error(defaultDrawable)
+                .resize(width, height)
+                .tag(context)
+                .into(imageView);
+    }
+}
