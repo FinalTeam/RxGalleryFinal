845263fc4025dec77b41044ea6614e7d798a9443
彭建波 <pengjianbo@finalteam.cn>
2016-07-28 00:29:45 +0800

    RxBus添加Sticky事件类型

--- library/src/main/java/cn/finalteam/rxgalleryfinal/rxbus/RxBus.java
+++ library/src/main/java/cn/finalteam/rxgalleryfinal/rxbus/RxBus.java
@@ -1,6 +1,10 @@
 package cn.finalteam.rxgalleryfinal.rxbus;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import rx.Observable;
+import rx.Subscriber;
 import rx.Subscription;
 import rx.subjects.PublishSubject;
 import rx.subjects.SerializedSubject;
@@ -16,12 +20,14 @@ public class RxBus {
 
     private static volatile RxBus mInstance;
     private final Subject<Object, Object> mBus;
+    private final Map<Class<?>, Object> mStickyEventMap;
 
     private final CompositeSubscription mSubscriptions;
 
     public RxBus() {
         mBus = new SerializedSubject<>(PublishSubject.create());
         mSubscriptions = new CompositeSubscription();
+        mStickyEventMap = new HashMap<>();
     }
 
     public static RxBus getDefault() {
@@ -108,5 +114,64 @@ public class RxBus {
      */
     public boolean hasSubscriptions() {
         return mSubscriptions.hasSubscriptions();
+    }
+
+
+    /**
+     * 发送一个新Sticky事件
+     */
+    public void postSticky(Object event) {
+        synchronized (mStickyEventMap) {
+            mStickyEventMap.put(event.getClass(), event);
+        }
+        post(event);
+    }
+
+    /**
+     * 根据传递的 eventType 类型返回特定类型(eventType)的 被观察者
+     */
+    public <T> Observable<T> toObservableSticky(final Class<T> eventType) {
+        synchronized (mStickyEventMap) {
+            Observable<T> observable = mBus.ofType(eventType);
+            final Object event = mStickyEventMap.get(eventType);
+
+            if (event != null) {
+                return Observable.merge(observable, Observable.create(new Observable.OnSubscribe<T>() {
+                    @Override
+                    public void call(Subscriber<? super T> subscriber) {
+                        subscriber.onNext(eventType.cast(event));
+                    }
+                }));
+            } else {
+                return observable;
+            }
+        }
+    }
+
+    /**
+     * 根据eventType获取Sticky事件
+     */
+    public <T> T getStickyEvent(Class<T> eventType) {
+        synchronized (mStickyEventMap) {
+            return eventType.cast(mStickyEventMap.get(eventType));
+        }
+    }
+
+    /**
+     * 移除指定eventType的Sticky事件
+     */
+    public <T> T removeStickyEvent(Class<T> eventType) {
+        synchronized (mStickyEventMap) {
+            return eventType.cast(mStickyEventMap.remove(eventType));
+        }
+    }
+
+    /**
+     * 移除所有的Sticky事件
+     */
+    public void removeAllStickyEvents() {
+        synchronized (mStickyEventMap) {
+            mStickyEventMap.clear();
+        }
     }
 }
