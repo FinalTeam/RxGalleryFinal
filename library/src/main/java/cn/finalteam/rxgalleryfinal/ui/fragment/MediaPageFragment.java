3d8de4dbffffef598143918b81ec38e7e5fb21a8
彭建波 <pengjianbo@finalteam.cn>
2016-07-30 21:55:43 +0800

    点击item后解决检查当前Page是否被选中

--- library/src/main/java/cn/finalteam/rxgalleryfinal/ui/fragment/MediaPageFragment.java
+++ library/src/main/java/cn/finalteam/rxgalleryfinal/ui/fragment/MediaPageFragment.java
@@ -107,6 +107,12 @@ public class MediaPageFragment extends BaseFragment implements ViewPager.OnPageC
                         mItemClickPosition = sendMediaPageFragmentDataEvent.getPosition();
                         mMediaBeanList.addAll(sendMediaPageFragmentDataEvent.getMediaBeanList());
                         mMediaPreviewAdapter.notifyDataSetChanged();
+                        MediaBean mediaBean = mMediaBeanList.get(mItemClickPosition);
+                        if(mMediaActivity != null && mMediaActivity.getCheckedList() != null){
+                            if(mMediaActivity.getCheckedList().contains(mediaBean)) {
+                                mCbCheck.setChecked(true);
+                            }
+                        }
 
                         RxBus.getDefault().post(new MediaViewPagerChangedEvent(mItemClickPosition, mMediaBeanList.size(), false));
                     }
