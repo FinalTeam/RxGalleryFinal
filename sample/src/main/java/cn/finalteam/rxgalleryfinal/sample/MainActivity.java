bedd250583cb2cf306b0e0c24b5f52bf41140b2f
Dean <abcruoshui@sina.com>
2018-11-12 15:37:30 +0800

    增加takePicCrop配置项
    用于画廊中选择相机拍摄照片后直接打开裁剪，而不是回到相册

--- sample/src/main/java/cn/finalteam/rxgalleryfinal/sample/MainActivity.java
+++ sample/src/main/java/cn/finalteam/rxgalleryfinal/sample/MainActivity.java
@@ -247,6 +247,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .radio()
                 .cropAspectRatioOptions(0, new AspectRatio("3:3", 30, 10))
                 .crop()
+                .takePicCrop()
                 .imageLoader(ImageLoaderType.FRESCO)
                 .subscribe(new RxBusResultDisposable<ImageRadioResultEvent>() {
                     @Override
